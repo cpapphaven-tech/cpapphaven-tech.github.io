a8fbe6207a778ee22b4f6ec9aef00888b45f9538
CP <cpapphaven@gmail.com>
2026-01-25 14:23:28 +0530

    added

---
+++ TrafficJam/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'traffic-v1.0.0';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './game.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
